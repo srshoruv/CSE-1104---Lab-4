b31a0f028a91086d702f8656a0538673b9d5006b
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2024-11-23 09:45:26 +0600

    problem solved

---
+++ numberInReverse.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+
+{   
+    printf("Printing the number in REVERSE\n");
+    printf("--------------------------------------\n");
+    
+    int num;
+    printf("Enter a number to reverse: ");
+    scanf("%d", &num);
+
+    int temp = num;
+    int rev = 0;
+
+    while (num)
+    {
+        long int lastDig = num % 10;
+        rev = rev * 10 + lastDig;
+        
+        
+        num = num / 10;
+    }
+
+    printf("%d in REVERSE is = %d\n",temp, rev);
+    
+    return 0;
+}
