8188335bacb6741804a1578eb44bc4b336050472
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2024-11-24 01:21:50 +0600

    minor changes

---
+++ frequencyFinder.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{   
+    printf("Finding the FREQUENCY of each digits\n");
+    printf("--------------------------------------\n");
+    
+    int num;
+    printf("Enter a number to start: ");
+    scanf("%d", &num);
+
+    
+    
+    return 0;
+}
