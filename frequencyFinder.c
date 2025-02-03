564c8429f85517ab68741091488b779bd8154116
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-02-03 22:32:41 +0600

    solved a new problem

--- frequencyFinder.c
+++ frequencyFinder.c
@@ -1,16 +1,29 @@
+// finding the frequency of each digit in a given integer
+
 #include <stdio.h>
 
 int main()
 
 {   
-    printf("Finding the FREQUENCY of each digits\n");
-    printf("--------------------------------------\n");
-    
-    int num;
+
+    int num, freq[10] = {0};
     printf("Enter a number to start: ");
     scanf("%d", &num);
 
-    
+    while (num != 0)
+    {
+        int lastDig = num % 10;
+        freq[lastDig]++;
+        num /= 10;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (freq[i] != 0)
+        {
+            printf("%d is appeared %d times\n", i, freq[i]);
+        }
+    }
     
     return 0;
 }
